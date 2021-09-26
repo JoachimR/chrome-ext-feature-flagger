09a4e0b005f3c5e9a4318aeac116fe78739d9693
JoachimReiss <reiss@bryter.io>
2021-09-26 22:03:00 +0200

    remove element-plus

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store, { key } from "../store";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 
 const app = createApp(App);
 
-app.use(ElementPlus);
 app.use(store, key);
 
 app.mount("#app");
